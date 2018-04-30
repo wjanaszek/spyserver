245836696c938aaff08bd8d9b57041a6879658f1
Michal Misiewicz <misiewicz.michall@gmail.com>
2018-04-30 19:55:20 +0200

    use dbcp for better db connections management. Close #2

---
+++ src/main/java/pl/edu/pw/elka/tin/spy/server/infrastructure/DBManager.java
@@ -0,0 +1,62 @@
+package pl.edu.pw.elka.tin.spy.server.infrastructure;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBManager {
+
+    private static final String DB_URL = "jdbc:h2:tcp://localhost//mnt/ubuntuDrive/h2test";
+    private static final String DB_USER = "admin";
+    private static final String DB_PASSWORD = "12345";
+    private static final String DB_DRIVER_CLASSNAME = "org.h2.Driver";
+
+    private static volatile DBManager dbManager;
+    private BasicDataSource dataSource;
+
+    private DBManager() {
+        dataSource = initDataSource();
+    }
+
+    public static DBManager manager() {
+        if (dbManager == null) {
+            synchronized (DBManager.class) {
+                if (dbManager == null) {
+                    dbManager = new DBManager();
+                }
+            }
+        }
+        return dbManager;
+    }
+
+    private static BasicDataSource initDataSource() {
+        BasicDataSource ds = new BasicDataSource();
+
+        ds.setDriverClassName(DB_DRIVER_CLASSNAME);
+        ds.setUrl(DB_URL);
+        ds.setUsername(DB_USER);
+        ds.setPassword(DB_PASSWORD);
+
+//        ds.setMaxWait(maxWaitMillis);
+//        ds.setMaxActive(maxTotal);
+
+        return ds;
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void shutdown() {
+        try {
+            dataSource.close();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/pl/edu/pw/elka/tin/spy/server/infrastructure/H2SpyRepository.java
+++ src/main/java/pl/edu/pw/elka/tin/spy/server/infrastructure/H2SpyRepository.java
@@ -6,50 +6,20 @@ import pl.edu.pw.elka.tin.spy.server.domain.SpyRepository;
 import pl.edu.pw.elka.tin.spy.server.domain.task.Task;
 import pl.edu.pw.elka.tin.spy.server.domain.task.TaskStatus;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
 @Slf4j
 public class H2SpyRepository implements SpyRepository {
     private final static H2SpyRepository instance = new H2SpyRepository();
-
-    private static final String DB_URL = "jdbc:h2:tcp://localhost//mnt/ubuntuDrive/h2test";
-    private static final String DB_USER = "admin";
-    private static final String DB_PASSWORD = "12345";
-
-
-    private Connection connection;
-
-    private H2SpyRepository() {
-        openConnection();
-    }
+    private final static DBManager dbManager = DBManager.manager();
 
     public static H2SpyRepository getInstance() {
         return instance;
-    }
-
-    public void openConnection() {
-        try {
-            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            connection.setAutoCommit(false);
-            log.info("Established connection to " + DB_URL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to open database connection");
-        }
-    }
-
-    public void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                connection = null;
-            }
-        }
     }
 
     @Override
@@ -57,6 +27,7 @@ public class H2SpyRepository implements SpyRepository {
         String selectTaskSQL = "SELECT * FROM TASK";
         List<Task> tasks = new LinkedList<>();
 
+        Connection connection = dbManager.getConnection();
         PreparedStatement stat;
         try {
             stat = connection.prepareStatement(selectTaskSQL);
